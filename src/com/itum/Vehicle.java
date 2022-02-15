f29bc1ddaf52173788ff3099c9613c4ba77f9406
Sathiskumar Nidharshan <nitharshan23s@gmail.com>
2022-02-15 19:27:50 +0530

    added java OOP concepts

---
+++ src/com/itum/Vehicle.java
@@ -0,0 +1,33 @@
+package com.itum;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Vehicle {
+    String modelYear="2000";
+    String brand="Toyota";
+
+    public void checkEngineStatus(){
+
+        System.out.println("Engine is running");
+    }
+}
+
+class Car extends Vehicle{
+    public Car(){
+        super();
+    }
+
+    public static void main(String[] args) {
+
+        String modelYear = "2020";
+        String brand = "Honda";
+
+
+        Car myCar = new Car();
+        myCar.checkEngineStatus();
+        System.out.println("Model Year: " + myCar.modelYear + "Brand: " + myCar.brand);
+
+    }
+
+
+}
